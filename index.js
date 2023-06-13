84ac34805f4ee56dda8decd4487096c832225d75
Alec Barker <avbbarker@icloud.com>
2023-06-13 10:26:56 -0400

    arithetic lab

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+let multiply = num1 * num2
+
+function multiply (){
+    return multiply
+}
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+function random(){
+    return random
+}
+
+const num3 = 14
+const num4 = 10
+
+let mod = num3 % num4
+
+function mod(){
+    return mod
+}
+
+let max = Math.max(1,5,10,15,20)
+
+function max(){
+    return max
+}
